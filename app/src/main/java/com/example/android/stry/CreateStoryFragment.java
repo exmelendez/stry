e6b91aba47d1337c69a0e4011b64b9ab6a8e945b
exmelenez <exmelendez@gmail.com>
2017-04-13 22:28:05 -0400

    initial story fab layout + 1st commit

---
+++ app/src/main/java/com/example/android/stry/CreateStoryFragment.java
@@ -0,0 +1,32 @@
+package com.example.android.stry;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class CreateStoryFragment extends Fragment {
+
+    private static final String TAG = "CreateStoryFragment";
+    private Button createStoryBtn;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.tab3_fragment,container,false);
+        createStoryBtn = (Button) view.findViewById(R.id.btnTest3);
+
+        createStoryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "TESTING BUTTON CLICK 3",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
+    }
+}
