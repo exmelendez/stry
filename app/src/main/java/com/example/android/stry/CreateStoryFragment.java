9b3eb93e7bfd1dd162cc6a58725753647238d0f4
exmelenez <exmelendez@gmail.com>
2017-04-14 14:55:29 -0400

    Added test dialog layouts + Java classes. Non-crashing, but still not fully functional.

--- app/src/main/java/com/example/android/stry/CreateStoryFragment.java
+++ app/src/main/java/com/example/android/stry/CreateStoryFragment.java
@@ -1,32 +1,62 @@
 package com.example.android.stry;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.widget.Button;
-import android.widget.Toast;
 
-public class CreateStoryFragment extends Fragment {
+public class CreateStoryFragment extends Activity {
 
     private static final String TAG = "CreateStoryFragment";
     private Button createStoryBtn;
 
-    @Nullable
+//    @Nullable
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//        View view = inflater.inflate(R.layout.create_story_fragment,container,false);
+//        createStoryBtn = (Button) view.findViewById(R.id.btnTest3);
+//
+//        createStoryBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(getActivity(), "TESTING BUTTON CLICK 3",Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//        return view;
+//    }
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.tab3_fragment,container,false);
-        createStoryBtn = (Button) view.findViewById(R.id.btnTest3);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_story_fragment);
+        Log.d(TAG, "onCreate: Starting.");
 
-        createStoryBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getActivity(), "TESTING BUTTON CLICK 3",Toast.LENGTH_SHORT).show();
-            }
-        });
+//        LinearLayout ll = (LinearLayout) findViewById(R.id.story_create_popup_layout);
+//        ll.setAlpha(0.1);
 
-        return view;
+//        createStoryBtn = (Button) findViewById(R.id.btnTest3);
+//
+//        createStoryBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(getActivity(), "TESTING BUTTON CLICK 3",Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width * .90), (int)(height * .85));
+//        getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+
+
     }
+
 }
